05e7df1ab6fb193797cefbe6ad509ab472754bdc
Lukas Bulin <luki.bulin05@gmail.com>
2026-09-21 21:10:13 -0700

    lab2 progress

---
+++ lab2.c
@@ -0,0 +1,37 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+char *saveptr = NULL;
+size_t len = 0;
+ssize_t n = 0;
+
+int main() {
+  while (1) {
+
+    printf("Enter programs to run.\n");
+    n = getline(&saveptr, &len, stdin);
+    if (n == -1) {
+      perror("getline has failed");
+      free(saveptr);
+      exit(EXIT_FAILURE);
+    }
+
+    saveptr[strcspn(saveptr, "\n")] = '\0';
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+      printf("Fork failed!\n");
+    } else if (pid == 0) {
+      printf("CHILD:\n");
+      execl(saveptr, saveptr, NULL);
+    } else {
+      printf("PARENT:\n");
+      waitpid(pid, NULL, 0);
+    }
+  }
+}
